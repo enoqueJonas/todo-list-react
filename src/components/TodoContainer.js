b53fe9b1181b25d0efe1c14a278e913225f7502d
enoqueJonas <enoque.macanda@gmail.com>
2022-12-12 18:49:45 +0200

    ADD react app to the repo

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello from react app!</h1>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import TodoContainer from "./components/TodoContainer";
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<TodoContainer />, root);
